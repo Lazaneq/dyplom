d93c5c246b5e7859c4cd98db373e9f3a28b8cef3
Lazaneq <lazanowski.artur@gmail.com>
2024-09-20 12:29:19 +0200

    Initial commit

---
+++ src/main/java/org/dyplom/aplikacja/AplikacjaDyplomApplication.java
@@ -0,0 +1,13 @@
+package org.dyplom.aplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaDyplomApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AplikacjaDyplomApplication.class, args);
+  }
+
+}
